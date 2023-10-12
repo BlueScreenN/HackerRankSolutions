b2a4b13b8d7ebdc6ccfe54b187e73be19fe47dbd
BlueScreenN <50810396+BlueScreenN@users.noreply.github.com>
2023-10-12 08:31:17 +0300

    Delete PrintFunction.py

--- PrintFunction.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    if 1 <= n and n <= 150:
-        rangeValue = range(1, n+1)
-        emptyString = ""
-        for i in rangeValue:
-            x = str(i)
-            emptyString += x
-        print(emptyString)
