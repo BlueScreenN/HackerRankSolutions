eab17c871335f0eebe899e6aa4d7c63aba63e392
OsiBK <osman_bayram-fb@hotmail.com>
2024-02-18 08:29:03 +0300

    What's your name solution

---
+++ Python/HackerRankSolutions/WhatsYourName.py
@@ -0,0 +1,7 @@
+def print_full_name(first, last):
+    print("Hello {} {}! You just delved into python.".format(first, last))
+
+if __name__ == '__main__':
+    name = input()
+    lastName = input()
+    print_full_name(name, lastName)
